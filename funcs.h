d75ed93d77559325ba650b60e4a35acacea616b4
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-03-30 21:07:11 -0400

    completed lab09

--- funcs.h
+++ funcs.h
@@ -1,3 +1,15 @@
 #pragma once
+#include "coord3d.h"
 
-// add prototypes here
+//task A
+double length(Coord3D *p);
+
+//task B
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2);
+
+//task C
+void move(Coord3D *ppos, Coord3D *pvel, double dt);
+
+//task E
+Coord3D* createCoord3D(double x, double y, double z);
+void deleteCoord3D(Coord3D *p);
